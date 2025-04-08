a4451cc49cbcf1e02fe8bbfeacd187ec107e8d58
FabrycioNeves <nevesfabrycio@gmail.com>
2025-04-08 14:43:47 -0300

    novo commit

--- Routes/Stack.js
+++ Routes/Stack.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./Tab";
 import Notificações from "../components/notificações/notificações";
