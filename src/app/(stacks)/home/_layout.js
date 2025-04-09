2115ab0539e594b2e25736421cf72a62e39c9eb1
FabrycioNeves <nevesfabrycio@gmail.com>
2025-04-09 17:52:05 -0300

    doiss

--- src/app/(stacks)/home/_layout.js
+++ src/app/(stacks)/home/_layout.js
@@ -12,7 +12,7 @@ export default function TabLayout() {
             }}
         >
             <Tabs.Screen
-                name="index"
+                name="home"
                 options={{
                     tabBarIcon: ({ color, size }) => (
                         <MaterialIcons name="home" size={size} color={color} />
